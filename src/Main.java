1c0140ad4b83b9cbab56c44e3422caa37957d195
SeveriusRasmussen <sora0001@stud.kea.dk>
2024-05-13 13:48:39 +0200

    First Commit 2024/05/13

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("""
+                               Welcome to the Dolphine Swimming Club
+                               1.
+                               2.
+                               3.
+                               4.
+                               
+                               
+                               
+                               """);
+            int choice = input.nextInt();
+
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Welcome");
+                    break;
+                case 2:
+                    System.out.println("WELCOME");
+                    break;
+                case 3:
+                    System.out.println("VELKOMMEN!");
+                    break;
+                case 4:
+                    System.out.println("YOU'RE ABANDONING ME?! I AM CRYING!");
+                    return;
+                default:
+                    System.out.println("WRONG INPUT! YOU WORD BLIND OR WHAT.");
+            }
+        }
+    }
+}
