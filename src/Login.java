7b7cc4e656f3282618754022138b0597d87f4a4d
lasse <laot0001@stud.kea.dk>
2024-05-15 14:42:41 +0200

    Added Login and UI class, updated Main with login calls

---
+++ src/Login.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Login {
+    ArrayList<Employee> users;
+    // create hashmap
+    HashMap<String,String> logininfo = new HashMap<String,String>();
+
+
+    Login(ArrayList<Employee> users){
+        this.users=users;
+    }
+
+    public HashMap<String,String> makeLogins(){
+        for(Employee user:users){
+            //if (user instanceof employee){
+            logininfo.put(user.getUsername(), user.getPassword());
+            //}
+        }
+        return logininfo;
+    }
+
+    public static Employee attemptLogin(HashMap<String,String> logininfo,ArrayList<Employee>users){
+        Scanner scan = new Scanner(System.in);
+        String userName;
+        //prompt for data
+        do{
+            System.out.println("Enter username: ");
+            userName = scan.nextLine();
+
+            //test if userName exists
+            if(userExists(userName,logininfo)) {
+                System.out.println("User exists");
+                break;
+            }
+            System.out.println("User does not exist, try again");
+        }while(!userExists(userName, logininfo));
+        Employee currentUser;
+        int loginAttempts=0;
+        do {
+            if (loginAttempts > 0) {
+                System.out.println("Wrong attempts: "+loginAttempts);
+            }
+            System.out.println("Enter password: ");
+            String passWord = scan.nextLine();
+            currentUser = loginSucces(userName, passWord, logininfo,users);
+            loginAttempts++;
+
+        }while(loginAttempts<3 && currentUser==null);
+        return currentUser;
+
+    }
+
+
+    public static Employee loginSucces(String userName,String enteredPassword,HashMap<String,String>logininfo,ArrayList<Employee>users){
+        Employee currentUser=null;
+        if(userExists(userName,logininfo)){
+            if(logininfo.get(userName).equals(enteredPassword)){
+                currentUser=getUser(userName,users);
+                System.out.println("Successfully logged in");
+                System.out.println("Welcome "+currentUser.getName());
+                return currentUser;
+            }
+        }
+        System.out.println("Wrong password, try again");
+        return null;
+    }
+
+    public static boolean userExists(String userName, HashMap<String,String>logininfo){
+        if(logininfo.containsKey(userName)){
+            return true;
+        }
+        return false;
+    }
+
+    public static Employee getUser(String userName,ArrayList<Employee>users){
+        for(Employee user:users){
+            if(user.getUsername().equals(userName)){
+                return user;
+            }
+
+        }
+        return null;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("""
-                               Welcome to the Dolphine Swimming Club
-                               1.
-                               2.
-                               3.
-                               4.
-                               LOL
-                               
-                               
-                               """);
-            int choice = input.nextInt();
+    public static void main(String[] args) throws Exception {
 
 
-            switch (choice) {
-                case 1:
-                    System.out.println("Welcome");
-                    break;
-                case 2:
-                    System.out.println("WELCOME");
-                    break;
-                case 3:
-                    System.out.println("VELKOMMEN!");
-                    break;
-                case 4:
-                    System.out.println("YOU'RE ABANDONING ME?! I AM CRYING!");
-                    return;
-                default:
-                    System.out.println("WRONG INPUT! YOU WORD BLIND OR WHAT.");
-            }
-        }
+        //making a list for the employees
+        ArrayList<Employee> employees = new ArrayList<>();
+        //Making some employees, and adding them to the list
+        employees.add(new Employee("Karsten","12345678","På gaden",58,1,"password","Formand","Karsten med stort K"));
+        employees.add(new Employee("Connie","87654321","Derhjemme ",41,3,"ConniePigen","Træner","Connie82"));
+        Scanner scan = new Scanner(System.in);
+
+
+        //make login object
+        Login newLogin = new Login(employees);
+
+        Employee currentUser = Login.attemptLogin(newLogin.makeLogins(),employees);
+        UI.userRole(currentUser);
     }
+
 }
